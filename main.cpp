c3f7c4b3497351b19b5be7dccf7a69359277db4d
Stefan Hagen <stefan@hagen.link>
2021-01-02 15:47:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <gtest/gtest.h>
-#include <fibonacci.h>
+#include <tribonacci.hpp>
 
 TEST(FibonacciTests, BasicChecks) {
-    ASSERT_TRUE(fibonacci(0) == 0);
-    ASSERT_TRUE(fibonacci(1) == 1);
-    ASSERT_TRUE(fibonacci(2) == 1);
-    ASSERT_TRUE(fibonacci(3) == 2);
-    ASSERT_TRUE(fibonacci(4) == 3);
-    ASSERT_TRUE(fibonacci(5) == 5);
-    ASSERT_TRUE(fibonacci(6) == 8);
-    ASSERT_TRUE(fibonacci(7) == 13);
-    ASSERT_TRUE(fibonacci(8) == 21);
-    ASSERT_TRUE(fibonacci(9) == 34);
-    ASSERT_TRUE(fibonacci(10) == 55);
-    ASSERT_TRUE(fibonacci(11) == 89);
-    ASSERT_TRUE(fibonacci(12) == 144);
-    ASSERT_TRUE(fibonacci(13) == 233);
+    // 0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, … (sequence A000073 in the OEIS)
+    ASSERT_TRUE(tribonacci(0) == 0);
+    ASSERT_TRUE(tribonacci(1) == 1);
+    ASSERT_TRUE(tribonacci(2) == 1);
+    ASSERT_TRUE(tribonacci(3) == 2);
+    ASSERT_TRUE(tribonacci(4) == 4);
+    ASSERT_TRUE(tribonacci(5) == 7);
+    ASSERT_TRUE(tribonacci(6) == 13);
+    ASSERT_TRUE(tribonacci(7) == 24);
+    ASSERT_TRUE(tribonacci(8) == 44);
+    ASSERT_TRUE(tribonacci(9) == 81);
+    ASSERT_TRUE(tribonacci(10) == 149);
+    ASSERT_TRUE(tribonacci(11) == 274);
+    ASSERT_TRUE(tribonacci(12) == 504);
+    ASSERT_TRUE(tribonacci(13) == 927);
 }
