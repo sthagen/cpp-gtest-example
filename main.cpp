3630bb00cde2397cd137929a7a726e7e0547ec96
Stefan Hagen <stefan@hagen.link>
2021-01-03 17:15:26 +0100

    Added test for dynamic programming implementation

--- main.cpp
+++ main.cpp
@@ -38,3 +38,22 @@ TEST(tribonacci_iter_tests, basic_values) {
     ASSERT_TRUE(tribonacci_iter(13) == 504);
     ASSERT_TRUE(tribonacci_iter(14) == 927);
 }
+
+TEST(tribonacci_dynamic_tests, basic_values) {
+    // 0, 0, 1, 1, 2, 4, 7, 13, 24, 44, 81, 149, 274, 504, 927, 1705, 3136, 5768, 10609, 19513, 35890, 66012, … (sequence A000073 in the OEIS)
+    ASSERT_TRUE(tribonacci_dynamic(0) == 0);
+    ASSERT_TRUE(tribonacci_dynamic(1) == 0);
+    ASSERT_TRUE(tribonacci_dynamic(2) == 1);
+    ASSERT_TRUE(tribonacci_dynamic(3) == 1);
+    ASSERT_TRUE(tribonacci_dynamic(4) == 2);
+    ASSERT_TRUE(tribonacci_dynamic(5) == 4);
+    ASSERT_TRUE(tribonacci_dynamic(6) == 7);
+    ASSERT_TRUE(tribonacci_dynamic(7) == 13);
+    ASSERT_TRUE(tribonacci_dynamic(8) == 24);
+    ASSERT_TRUE(tribonacci_dynamic(9) == 44);
+    ASSERT_TRUE(tribonacci_dynamic(10) == 81);
+    ASSERT_TRUE(tribonacci_dynamic(11) == 149);
+    ASSERT_TRUE(tribonacci_dynamic(12) == 274);
+    ASSERT_TRUE(tribonacci_dynamic(13) == 504);
+    ASSERT_TRUE(tribonacci_dynamic(14) == 927);
+}
